1e55986cc24030b35b9c1823910d773374412714
OTHSCS106 <OTHSCS106@KATYISD.ORG>
2019-10-14 08:14:59 -0500

    Finished adding data to program and preprocessing data

--- cnn.py
+++ cnn.py
@@ -1,15 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import mnist
-import scipy.misc
 
+def convolution(X, F, step_size):
+    return X
+
+
+#60000 samples
 X_train = mnist.train_images()
 Y_train = mnist.train_labels()
 
-scipy.misc.toimage(scipy.misc.imresize(X_train[0,:,:] * -1 + 256, 10.))
-
+#10000 sample
 X_test = mnist.test_images()
 Y_test = mnist.test_labels()
+
+X_train = X_train / 255
+X_test = X_test / 255
+
+F1 = np.random.rand(5,5)
+
+convolution(X_train[0], F1, 1)
+print(F1)
+
 
 
 
