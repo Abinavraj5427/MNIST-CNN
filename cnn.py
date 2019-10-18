aadcc2ff8d9daf848058f9b48877cf65296e90ef
OTHSCS106 <OTHSCS106@KATYISD.ORG>
2019-10-18 07:59:17 -0500

    Finished convolution

--- cnn.py
+++ cnn.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 import mnist
 
 def convolution(X, F, step_size):
-    for r in range(28):
-        for c in range(28):
+    C = np.zeros((X.shape[0], X.shape[1]-F.shape[0]+1, X.shape[2]-F.shape[1]+1))
+    for r in range(28- (F.shape[0]-1)):
+        for c in range(28-(F.shape[1]-1)):
+            C[:,r,c] = (X[:,r:r+F.shape[0],c:c+F.shape[1]] * F).sum()
+    return C
             
 
 
@@ -20,9 +23,9 @@ X_train = X_train / 255
 X_test = X_test / 255
 
 F1 = np.random.rand(5,5)
-
-convolution(X_train[0], F1, 1)
-print(F1)
+#print(X_train[0:2].shape)
+C1 = convolution(X_train[0:2], F1, 1)
+#print(C1.shape)
 
 
 
